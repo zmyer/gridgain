31f6211a3e1c1c4515b20104d8699565bb73eceb
sergeyuttsel <uttsel@gmail.com>
2020-04-08 22:08:47 +0300

    WIP

--- modules/core/src/test/java/org/apache/ignite/internal/util/IgniteUtilsWorkDirectoryTest.java
+++ modules/core/src/test/java/org/apache/ignite/internal/util/IgniteUtilsWorkDirectoryTest.java
@@ -81,6 +81,38 @@ public class IgniteUtilsWorkDirectoryTest {
      * The work directory specified by the user has the highest priority
      */
     @Test
+    public void sunNewTests_1() {
+        System.out.println("q");
+    }
+
+    /**
+     * The work directory specified by the user has the highest priority
+     */
+    @Test
+    public void sunNewTests_2() {
+        throw new RuntimeException();
+    }
+
+    /**
+     * The work directory specified by the user has the highest priority
+     */
+    @Test
+    public void sunNewTests_3() {
+        System.out.println("q");
+    }
+
+    /**
+     * The work directory specified by the user has the highest priority
+     */
+    @Test
+    public void sunNewTests_4() {
+        throw new RuntimeException();
+    }
+
+    /**
+     * The work directory specified by the user has the highest priority
+     */
+    @Test
     public void testWorkDirectory1() {
         executeGenericTest(true, false, false,
             USER_WORK_DIR);
